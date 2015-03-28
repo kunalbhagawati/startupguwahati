8c40a53a930ec5f22b7f2c678db78c3f7a0bdf89
Kunal <kunal.bhagawati@gmail.com>
2015-03-29 00:03:28 +0530

    URL bug fixes

--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = patterns('',
-    url(r'', views.IndexView.as_view()),
+    url(r'^$', views.IndexView.as_view()),
     url(r'^teapot/(?P<potType>[1234]{0,1})$', views.teapotView, name="teapot"),
     url(r'^admin/', include(admin.site.urls)),
 )
