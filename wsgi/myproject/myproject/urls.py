b666cae570768453ac2522098ccef6dab769ddbd
Kunal <kunal.bhagawati@gmail.com>
2015-03-28 22:11:33 +0530

    EA

--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = patterns('',
+    url(r'^teapot/(?P<potType>[1234]{0,1})$', views.teapotView, name="teapot"),
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ wsgi/myproject/myproject/views.py
@@ -0,0 +1,28 @@
+# # REST Framework
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET', ])
+def teapotView(request, potType):
+    """
+    Implements the HTCPCP (HyperText Coffee Pot Control Protocol)
+
+    http://en.wikipedia.org/wiki/Hyper_Text_Coffee_Pot_Control_Protocol
+
+    Sends back a standard 418 response code
+    
+    """
+    
+    if potType == '':
+        potType = '1'
+
+    potTypes = {
+        '1' : " ◝(‿)𝀍 ", 
+        '2' : " \{_}7 ", 
+        '3' : " ◝⋃𝀍 ", 
+        '4' : " ◝⋃⁊ ", 
+        }
+
+    response = ' Coffee at your service! ' + potTypes[potType]
+    return Response(response, status=418)
