9c3575f15b9e99c6493a3bf3b20c1ba0c3086bb5
Kunal <kunal.bhagawati@gmail.com>
2015-05-10 03:40:51 +0530

    Added urls for user

--- wsgi/StartUpGuwahati/StartUpGuwahati/urls.py
+++ wsgi/StartUpGuwahati/StartUpGuwahati/urls.py
@@ -3,25 +3,26 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from . import restViews, djangoViews
 
-# urlpatterns = patterns('',
-#     url(r'^api/', include(patterns('',
-#         # url(r'^teapot/(?P<potType>[1234]{0,1})$',
-#         #         restViews.teapotView, name="teapot"),
-#         # url(r'^twitterreader', include('twitterreader.restUrls')),
-#         url(r'^places', include('places.restUrls')),
-#         ),
-#     ), ), )
-
 urlpatterns = patterns('',
+    url(r'^api/', include(patterns('',
+        url(r'^teapot/(?P<potType>[1234]{0,1})$',
+                restViews.teapotView, name="teapot"),
+        # url(r'^twitterreader', include('twitterreader.restUrls')),
+        url(r'^places/', include('places.restUrls')),
+        url(r'^users/', include('users.restUrls')),
+        ),
+    ), ), )
+
+urlpatterns += patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT}),
 )
 
 urlpatterns += patterns('',
-    # url(r'^$', djangoViews.IndexView.as_view()),
+    url(r'^$', djangoViews.IndexView.as_view()),
     # url(r'^twitterreader', include('twitterreader.djangoUrls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^places/', include('places.restUrls')),
-    url(r'^users/', include('users.restUrls')),
-    # (r'^foundation/', include('foundation.urls')),
+    # url(r'^places/', include('places.restUrls')),
+    url(r'^users/', include('users.urls')),
+    (r'^foundation/', include('foundation.urls')),
 )
